7da7c899572c5b96f7cd5e795df4951db6fa8e82
Pontus Magnusson <pontuspdmagnusson@gmail.com>
2015-04-18 00:31:11 +0200

    Update

--- Synced/Synced/Static Classes/InputManager.cs
+++ Synced/Synced/Static Classes/InputManager.cs
@@ -9,7 +9,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Synced.InputCommands;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework;
 using Synced.Game_Actors;
