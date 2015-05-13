1962857851f6cec6f0357ef0b5f6a452991c9541
Dennis Stockhaus <destarfighter@gmail.com>
2015-05-13 15:55:06 +0200

    Another commit

--- Synced/Synced/InGame/Actors/CollidingSprite.cs
+++ Synced/Synced/InGame/Actors/CollidingSprite.cs
@@ -9,20 +9,48 @@ using Synced.InGame;
 using Synced.Static_Classes;
 using FarseerPhysics.Dynamics;
 using FarseerPhysics.Factories;
+using FarseerPhysics.Collision.Shapes;
+using FarseerPhysics;
 
 namespace Synced.InGame.Actors
 {
-    class CollidingSprite : Synced.Actors.Sprite
+    abstract class CollidingSprite : Synced.Actors.Sprite
     {
         #region Variables
-
+        // FarseerPhysics variables
+        protected World world;
+        protected Body body;
+        protected Shape shape;
+        protected Fixture fixture;
+        // Collision info variables
+        // Tag etc. 
         #endregion
 
         #region Properties
         #endregion
 
-        public CollidingSprite(Texture2D texture, Vector2 position, DrawingHelper.DrawingLevel drawingLevel, Game game)
-        : base(texture, position, drawingLevel, game)
-        { }
+        public CollidingSprite(Texture2D texture, Vector2 position, DrawingHelper.DrawingLevel drawingLevel, Game game, World world)
+            : base(texture, position, drawingLevel, game)
+        {
+            this.world = world;
+            body = new Body(world, ConvertUnits.ToSimUnits(position), 0f);
+
+        }
+
+        virtual public bool OnCollision() { return true; }
+
+        public override void Draw(GameTime gameTime)
+        {
+            // set display ratio
+            //ConvertUnits.SetDisplayUnitToSimUnitRatio(10f);
+            //float width = ConvertUnits.ToSimUnits((float)ResolutionManager.GetWidth);
+            //float height = ConvertUnits.ToSimUnits((float)ResolutionManager.GetHeight);
+
+            _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.LinearWrap, DepthStencilState.None, RasterizerState.CullNone, null, ResolutionManager.GetTransformationMatrix());
+            _spriteBatch.Draw(Texture, ConvertUnits.ToDisplayUnits(body.Position)/1000, null, Color, body.Rotation, Origin, 1.0f, SpriteEffects.None, 1.0f); // TODO: use body pos/rot or Sprite pos/rot? 
+            _spriteBatch.End();
+
+            Position = ConvertUnits.ToDisplayUnits(body.Position);
+        }
     }
 }
