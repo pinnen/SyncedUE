dbdfc6bcf08fcb8174f0733924400facabc6a282
Dennis Stockhaus <destarfighter@gmail.com>
2015-05-13 11:39:38 +0200

    CollidingSprite
    Added Class CollidingSprite

---
+++ Synced/Synced/InGame/Actors/CollidingSprite.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Synced.Content;
+using Synced.InGame;
+using Synced.Static_Classes;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+
+namespace Synced.InGame.Actors
+{
+    class CollidingSprite : Synced.Actors.Sprite
+    {
+        #region Variables
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        public CollidingSprite(Texture2D texture, Vector2 position, DrawingHelper.DrawingLevel drawingLevel, Game game)
+        : base(texture, position, drawingLevel, game)
+        { }
+    }
+}
