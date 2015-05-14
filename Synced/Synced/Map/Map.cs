7a25939fa431c277bdad8331104b532e05924538
pinnen <pontuspdmagnusson@gmail.com>
2015-05-14 02:18:06 +0200

    Test

--- Synced/Synced/Map/Map.cs
+++ Synced/Synced/Map/Map.cs
@@ -7,6 +7,8 @@
 using FarseerPhysics.Common;
 using FarseerPhysics.Dynamics;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Synced.Actors;
 using Synced.Content;
 using System;
 using System.Collections.Generic;
@@ -17,25 +19,47 @@ namespace Synced.MapNamespace
     class Map
     {
         #region Variables
+        List<GameComponent> _components;
+        #endregion
+        #region Properties
         public MapData Data
         {
             get;
-            private set;
+            set;
         }
-        #endregion
-        #region Properties
-        
         public World World
         {
             get;
             set;
         }
 
-        public Map(string path)
+        public Map(string path, Game game)
         {
+            _components = new List<GameComponent>();
             Data = Library.Serialization<MapData>.DeserializeFromXmlFile(path);
-
             World = new World(Vector2.Zero); // Topdown games have no gravity
+
+            // Process data
+            foreach (var mapObject in Data.Objects)
+            {
+                if (mapObject is Obstacle)
+                {
+                    _components.Add(new Sprite(game.Content.Load<Texture2D>(mapObject.TexturePath), mapObject.Position, Static_Classes.DrawingHelper.DrawingLevel.Back, game));
+                }
+                else if (mapObject is Goal)
+                {
+                    _components.Add(new Sprite(game.Content.Load<Texture2D>(mapObject.TexturePath), mapObject.Position, Static_Classes.DrawingHelper.DrawingLevel.Back, game));
+                }
+                else if (mapObject is PlayerStart)
+                {
+                    _components.Add(new Sprite(game.Content.Load<Texture2D>(mapObject.TexturePath), mapObject.Position, Static_Classes.DrawingHelper.DrawingLevel.Back, game));
+                }
+                else if (mapObject is MapObject)
+                {
+                    _components.Add(new Sprite(game.Content.Load<Texture2D>(mapObject.TexturePath), mapObject.Position, Static_Classes.DrawingHelper.DrawingLevel.Back, game));
+                }
+            }
+            _components.ForEach(x => game.Components.Add(x));
         }
         #endregion
     }
