7e756d9afd88300f5d6efc104b3ce16368deae98
bobobear91 <rcd10002@student.mdh.se>
2015-05-18 22:15:41 +0200

    screenmanager

--- Synced/Synced/Interface/MenuScreen.cs
+++ Synced/Synced/Interface/MenuScreen.cs
@@ -32,26 +32,28 @@ namespace Synced.Interface
         public MenuScreen(Texture2D texture, Game game)
             : base(game)
         {
-            _background = new Sprite(texture, Vector2.Zero, DrawingHelper.DrawingLevel.Back, game);
-            _background.Enabled = false;
-            _background.Visible = false;
+            //_background = new Sprite(texture, Vector2.Zero, DrawingHelper.DrawingLevel.Back, game);
+            //_background.Enabled = false;
+            //_background.Visible = false;
+            GameComponents.Add(new Sprite(texture, Vector2.Zero, DrawingHelper.DrawingLevel.Back, game));
+
 
             // Temporary screen variables (Half of screen)
             int w = ResolutionManager.GetCenterPointWidth;
             int h = ResolutionManager.GetCenterPointHeight;
 
             // Add character selectors
-            _characterSelectors = new List<CharacterSelector>();
-            _characterSelectors.Add(new CharacterSelector(PlayerIndex.One, new Rectangle(0, 0, w, h), Color.Blue, Game));
-            _characterSelectors.Add(new CharacterSelector(PlayerIndex.Two, new Rectangle(w, 0, w, h), Color.Green, Game));
-            _characterSelectors.Add(new CharacterSelector(PlayerIndex.Three, new Rectangle(0, h, w, h), Color.Red, Game));
-            _characterSelectors.Add(new CharacterSelector(PlayerIndex.Four, new Rectangle(w, h, w, h), Color.Yellow, Game));
-            foreach (CharacterSelector characterSelect in _characterSelectors)
-            {
-                characterSelect.Enabled = false;
-                characterSelect.Visible = false;
-            }
-
+            //_characterSelectors = new List<CharacterSelector>();
+            GameComponents.Add(new CharacterSelector(PlayerIndex.One, new Rectangle(0, 0, w, h), Color.Blue, Game));
+            GameComponents.Add(new CharacterSelector(PlayerIndex.Two, new Rectangle(w, 0, w, h), Color.Green, Game));
+            GameComponents.Add(new CharacterSelector(PlayerIndex.Three, new Rectangle(0, h, w, h), Color.Red, Game));
+            GameComponents.Add(new CharacterSelector(PlayerIndex.Four, new Rectangle(w, h, w, h), Color.Yellow, Game));
+            //foreach (CharacterSelector characterSelect in _characterSelectors)
+            //{
+            //    characterSelect.Enabled = false;
+            //    characterSelect.Visible = false;
+            //}
+            
                 
             //GameComponents.Add(new CharacterSelector(PlayerIndex.One, new Rectangle(0, 0, w, h), Color.Blue, Game));
             //GameComponents.Add(new CharacterSelector(PlayerIndex.Two, new Rectangle(w, 0, w, h), Color.Green, Game));
@@ -60,7 +62,7 @@ namespace Synced.Interface
             Game.Components.Add(_background);
             Game.Components.Add(this);
         }
-
+        
         public override void Initialize()
         {
             base.Initialize();
@@ -83,11 +85,6 @@ namespace Synced.Interface
         {
             _background.Enabled = true;
             _background.Visible = true;
-            foreach (CharacterSelector characterSelect in _characterSelectors)
-            {
-                characterSelect.Enabled = false;
-                characterSelect.Visible = false;
-            }
             base.Activated();
    
         }
