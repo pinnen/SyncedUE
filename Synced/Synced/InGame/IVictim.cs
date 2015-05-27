50a457d83518928977ae5e0792291141f364fb5f
Lina <linajuu@hotmail.com>
2015-05-27 23:13:23 +0200

    added IVictim interface

---
+++ Synced/Synced/InGame/IVictim.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Synced.InGame.Actors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Synced.InGame
+{
+    interface IVictim
+    {
+        float CircleEffectTimer { get; set; }
+        float TriangleEffectTimer { get; set; }
+        float HexagonEffectTimer { get; set; }
+        float PentagonEffectTimer { get; set; }
+        Texture2D VictimTexture { get; }
+        Color Color { get; }
+        float ParticleLifetime { get; }
+        float LocalTimeScale { get; set; }
+        Vector2 Position { get; set; }
+        float Rotation { get; set;}
+        Vector2 Direction { get; set; }
+        bool FadeOut { get; set; }
+        float InvisibilityAlpha { get; set; }
+        ParticleEngine TrailEngine { get; }
+
+    }
+}
