80dbc2cfcef937cdd875039761747c1fd05c39ca
bobobear91 <rcd10002@student.mdh.se>
2015-05-28 01:31:29 +0200

    Added back button back

--- Synced/Synced/SyncedGame.cs
+++ Synced/Synced/SyncedGame.cs
@@ -94,8 +94,12 @@ namespace Synced
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape) && _lastState.IsKeyUp(Keys.Escape))
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape) && _lastState.IsKeyUp(Keys.Escape))
                 ScreenManager.Instance.HandleBackEvent();
+
+            //TEMP EXIT FOR GAMEPADS
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                Exit();
             #region Debug
             if (Keyboard.GetState().IsKeyDown(Keys.F11) && _lastState.IsKeyUp(Keys.F11))
             {
