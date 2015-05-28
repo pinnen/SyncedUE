3982b8345d40f9a121d87dec448f17f8da30c929
Dennis <dss10001@student.mdh.se>
2015-05-28 12:57:46 +0200

    Map Update

---
+++ Synced/Synced/Map/BorderData.cs
@@ -0,0 +1,21 @@
+﻿using FarseerPhysics.Dynamics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Synced.CollisionShapes;
+using Synced.Static_Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Synced.MapNamespace
+{
+    [Serializable]
+    class BorderData : MapObjectData
+    {
+        public override GameComponent GetComponent(Game game, World world)
+        {
+            return new TexturePolygon(game.Content.Load<Texture2D>(TexturePath), Position, 0, (DrawingHelper.DrawingLevel)drawingLevel, game, world, false);
+        }
+    }
+}
