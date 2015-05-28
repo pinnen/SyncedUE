b79739ef9103adbe2d9acca69c9f5c506b898b60
Dennis <dss10001@student.mdh.se>
2015-05-28 11:30:42 +0200

    ResetGame func

--- Synced/Synced/Interface/GameScreen.cs
+++ Synced/Synced/Interface/GameScreen.cs
@@ -83,6 +83,12 @@ namespace Synced.Interface
             world.Step(Math.Min((float)gameTime.ElapsedGameTime.TotalSeconds, (1f / 30f)));
             base.Update(gameTime);
         }
+
+        public void ResetGame()
+        {
+            SyncedGameCollection.ComponentCollection.Clear();
+            world.Clear();
+        }
     }
 }
 
