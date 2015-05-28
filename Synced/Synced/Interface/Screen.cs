35f6ae95e5bab1794763cb8ac55c59275ac46573
bobobear91 <rcd10002@student.mdh.se>
2015-05-28 14:30:10 +0200

    Passing game data from menu to game

--- Synced/Synced/Interface/Screen.cs
+++ Synced/Synced/Interface/Screen.cs
@@ -23,7 +23,7 @@ namespace Synced.Interface
         public delegate void OnScreenDeactivateEventHandler(Screen screen, EventArgs e);
         public delegate void OnScreenTransitionEventHandler(Screen screen, EventArgs e);
         public delegate void OnScreenExitEventHandler(Screen screen, EventArgs e);
-        public delegate void StartNewGame(Screen screen, EventArgs e);
+        public delegate void StartNewGame(MenuScreen screen, EventArgs e);
 
         public event OnScreenActivateEventHandler OnActivated;
         public event OnScreenDeactivateEventHandler OnDeactivated;
