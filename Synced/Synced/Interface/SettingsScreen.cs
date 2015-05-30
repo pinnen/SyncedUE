fca8d8c9d68b8d809a64a848c127074d2337563e
bobobear91 <rcd10002@student.mdh.se>
2015-05-30 18:22:17 +0200

    SettingsScreen added (Empty)

---
+++ Synced/Synced/Interface/SettingsScreen.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Synced.Interface
+{
+    class SettingsScreen : Screen
+    {
+        public SettingsScreen(Game game) : base(game)
+        {
+
+
+        }
+    }
+}
